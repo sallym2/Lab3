2b9206168155da792ab2fe6f4d0f8c4a522b4ed2
Sally Ming <sallym2@illinois.edu>
2018-02-06 16:50:34 -0600

    Finished but can't test so not sure if it's correct the wordCount.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,36 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 public class WebScraper {
     public static void main(String[] unused) {
         System.out.println("here");
     }
+
+/**
+ * Retrieve contents from a URL and return them as a string.
+ *
+ * @param url url to retrieve contents from
+ * @return the contents from the url as a string, or an empty string on error
+ */
+public static String urlToString(final String url) {
+    Scanner urlScanner;
+    try {
+        urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+    } catch (IOException e) {
+        return "";
+    }
+    String contents = urlScanner.useDelimiter("\\A").next();
+    urlScanner.close();
+    return contents;
+}
+public static String wordCount(String website) {
+    String sentence = urlToString(website);
+    String[] words = sentence.split("[,./+;:\\[\\]\\-\\']+");
+    int wordCounter = 0;
+    for (int i = 0; i < words.length; i++) {
+        wordCounter++;
+    }
+    System.out.println(wordCounter);
+    return sentence;
+}
 }
